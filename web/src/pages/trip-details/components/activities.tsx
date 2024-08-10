d6a1b1a6a07880988e531d41f5fd7c1e4e580846
Lucas Felipe <lucasfelipemk@gmail.com>
2024-08-09 23:27:57 -0300

    :sparkles: feat: Getting activities data of API

--- web/src/pages/trip-details/components/activities.tsx
+++ web/src/pages/trip-details/components/activities.tsx
@@ -1,41 +1,78 @@
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { CircleCheck } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { api } from '@/lib/axios'
+
+interface Activity {
+  date: string
+  activities: {
+    id: string
+    title: string
+    occurs_at: string
+  }[]
+}
 
 export function Activities() {
+  const { tripId } = useParams()
+  const [activities, setActivities] = useState<Activity[]>([])
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data))
+  }, [tripId])
+
   return (
     <div className="space-y-8">
-      <article className="space-y-2.5">
-        <span className="flex items-baseline gap-2">
-          <span className="text-xl font-semibold text-zinc-300">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sábado</span>
-        </span>
+      {activities.map((category) => {
+        return (
+          <article key={category.date} className="space-y-2.5">
+            <span className="flex items-baseline gap-2">
+              <span className="text-xl font-semibold text-zinc-300">
+                Dia {format(category.date, 'd', { locale: ptBR })}
+              </span>
+              <span className="text-xs text-zinc-500">
+                {format(category.date, 'EEEE')}
+              </span>
+            </span>
 
-        <p className="text-sm text-zinc-500">
-          Nenhuma atividade cadastrada nessa data.
-        </p>
-      </article>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <article className="shadow-shape flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
 
-      <article className="space-y-2.5">
-        <span className="flex items-baseline gap-2">
-          <span className="text-xl font-semibold text-zinc-300">Dia 18</span>
-          <span className="text-xs text-zinc-500">Domingo</span>
-        </span>
+                        <span className="ml-auto text-sm text-zinc-400">
+                          {format(activity.occurs_at, 'HH:mm')}h
+                        </span>
+                      </article>
 
-        <div className="space-y-2.5">
-          <article className="shadow-shape flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
+                      <article className="shadow-shape flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">Almoço</span>
 
-            <span className="ml-auto text-sm text-zinc-400">08:00</span>
+                        <span className="ml-auto text-sm text-zinc-400">
+                          12:00
+                        </span>
+                      </article>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (
+              <p className="text-sm text-zinc-500">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
+            )}
           </article>
-
-          <article className="shadow-shape flex items-center gap-3 rounded-xl bg-zinc-900 px-4 py-2.5">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Almoço</span>
-
-            <span className="ml-auto text-sm text-zinc-400">12:00</span>
-          </article>
-        </div>
-      </article>
+        )
+      })}
     </div>
   )
 }
